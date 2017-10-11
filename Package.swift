f9d29871a6c1e7781db2273ec61649bb64af6dd2
Jonathan Guthrie <jono@perfect.org>
2017-10-11 17:18:01 -0400

    update for swift 4, perfect 3

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-13 19:06:51 +0000
+// Date: 2017-10-11 21:11:55 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectTurnstileMySQLDemo",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-MySQL.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1, minor: 0),
-    ]
+	name: "PerfectTurnstileMySQLDemo",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-MySQL.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
+	]
 )
